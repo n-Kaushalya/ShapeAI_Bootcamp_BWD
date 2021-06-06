fb13913d0e41e18bbd7ec09cdc1ffb1d05a39ae7
Nipuna Kaushalya <78372632+n-Kaushalya@users.noreply.github.com>
2021-06-07 00:24:47 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        I am Nipuna Kaushalya, an undergraduate student.This was an amazing
+        bootcamp taken up by Shaurya Sir. We coverd everything from Scratch
+        including React.js, JavaScript, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
